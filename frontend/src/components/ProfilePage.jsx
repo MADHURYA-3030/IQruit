39289f21577a7b9842d5b905d2d04f1a1dd64e59
Deekshitha Kammela <kammeladeekshitha@gmail.com>
2025-10-09 11:42:19 +0530

    Profile Page

---
+++ frontend/src/components/ProfilePage.jsx
@@ -0,0 +1,44 @@
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const ProfilePage = () => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+
+    if (!token) {
+      navigate("/login"); // redirect if not logged in
+      return;
+    }
+
+    axios
+      .get("http://localhost:5000/api/auth/profile", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => setUser(res.data))
+      .catch((err) => {
+        console.log(err);
+        alert("Session expired. Please login again.");
+        localStorage.removeItem("token");
+        navigate("/login");
+      });
+  }, [navigate]);
+
+  if (!user) return <p>Loading profile...</p>;
+
+  return (
+    <div className="page">
+      <div className="profile-container">
+        <h2>My Profile</h2>
+        <p><strong>Name:</strong> {user.name}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
